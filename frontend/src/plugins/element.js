568da8a3141b6a7ce5a34fc236e9a9160e53f8cc
FlameSky-S <FlameSky-S@users.noreply.github.com>
2020-10-30 14:23:52 +0800

    use English version of element-ui

--- frontend/src/plugins/element.js
+++ frontend/src/plugins/element.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/display.css'
+import locale from 'element-ui/lib/locale/lang/en'
 
 import '@/styles/element-variables.scss'
 
 Vue.use(ElementUI, {
   size: 'small',
+  locale,
 })
