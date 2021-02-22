e4455c9b873ce3b87d0881738afcb0d79bd366a7
FlameSky-S <FlameSky-S@users.noreply.github.com>
2021-02-22 17:22:29 +0800

    make baseURL editable after packing

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -3,7 +3,7 @@ const config = {
     // 'http://166.111.138.96:5000',
     process.env.NODE_ENV === 'development'
       ? 'http://166.111.138.98:5000'
-      : 'http://166.111.138.98:5000',
+      : window.baseURL,
   contentType: 'application/json;charset=UTF-8',
   successCode: [200, 0],
   requestTimeout: 60000,
